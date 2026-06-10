887871f6868e0a4a73ad1fea99ded0ec319f5bc8
Masterpro32 <N00481308@upn.pe>
2026-06-09 23:51:06 -0500

    Add files via upload

---
+++ Lab6/utils.py
@@ -0,0 +1,12 @@
+def bienvenida():
+    """
+    Muestra un mensaje inicial de bienvenida al usuario.
+
+    Parámetros:
+        Ninguno
+
+    Retorno:
+        None
+    """
+    # Mensaje que se muestra al iniciar el programa
+    print("Bienvenido al programa de funciones reutilizables.")
